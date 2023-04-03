81b6439d2bd5ccfee8f3e69c98a2e5811858d292
Grigoriy <kapranovgu@mail.ru>
2023-04-03 12:02:13 +0300

    Task 36 modified0

--- task36.py
+++ task36.py
@@ -23,3 +23,5 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
             num = operation(x, y)
             nums.append(num)
         print('\t\t'.join([str(x) for x in nums]))
+
+print_operation_table(lambda x, y: x * y) 
